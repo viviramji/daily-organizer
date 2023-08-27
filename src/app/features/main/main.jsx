2eb61f58e98b3e8c45760559622380a4cf74c954
Victor Victor RJ <wrjr04@gmail.com>
2023-08-26 21:33:54 -0500

    Add router to project.

--- src/app/features/main/main.jsx
+++ src/app/features/main/main.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { Provider } from "react-redux";
+import { Router, Route } from "react-router-dom";
 
 import { store } from "../../store";
 import { ConnectedDashboard } from "../dashboard/dashboard";
+import { history } from "../../store/history";
 
 export const Main = () => (
-  <Provider store={store}>
-    <div>
-      <ConnectedDashboard />
-    </div>
-  </Provider>
+  <Router history={history}>
+    <Provider store={store}>
+      <div>
+        {/* <ConnectedDashboard /> */}
+
+        <Route exact path="/dashboard" render={() => <ConnectedDashboard />} />
+
+      </div>
+    </Provider>
+  </Router>
 );
