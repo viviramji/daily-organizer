fd866fbbe26b75bcfd7be24018dc261733bc6cc9
Victor Victor RJ <wrjr04@gmail.com>
2023-08-26 21:41:34 -0500

    Add navigation feature and clean index.hml file.

--- src/app/features/main/main.jsx
+++ src/app/features/main/main.jsx
@@ -3,14 +3,16 @@ import { Provider } from "react-redux";
 import { Router, Route } from "react-router-dom";
 
 import { store } from "../../store";
-import { ConnectedDashboard } from "../dashboard/dashboard";
 import { history } from "../../store/history";
+import { ConnectedDashboard } from "../dashboard/dashboard";
+import { ConnectedNavigation } from "../navigation/navigation";
 
 export const Main = () => (
   <Router history={history}>
     <Provider store={store}>
       <div>
         {/* <ConnectedDashboard /> */}
+        <ConnectedNavigation />
 
         <Route exact path="/dashboard" render={() => <ConnectedDashboard />} />
 
