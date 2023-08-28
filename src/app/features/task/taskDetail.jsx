aa34fc82daf8db9d83f281f526e9b4a5973b999b
Victor Victor RJ <wrjr04@gmail.com>
2023-08-28 14:10:19 -0500

    Add actions to update task details.

--- src/app/features/task/taskDetail.jsx
+++ src/app/features/task/taskDetail.jsx
@@ -2,22 +2,35 @@ import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
-const TaskDetail = ({ id, task, isCompleted, comments, groups }) => (
+import * as mutations from "../../store/mutations";
+
+const TaskDetail = ({
+  id,
+  task,
+  isComplete,
+  comments,
+  groups,
+  setTaskCompletion,
+  setTaskGroup,
+  setTaskName,
+}) => (
   <div>
     <div>
-      <input value={task.name} />
+      <input onChange={setTaskName} value={task.name} />
     </div>
     <div>
-      <button>Complete / Reopen Task </button>
+      <button onClick={() => setTaskCompletion(id, !isComplete)}>
+        {isComplete ? "Reopen" : "Complete"}
+      </button>
     </div>
 
     <div>
-      <select>
-        {
-          groups.map(group => (
-            <option key={group.id} value={group.id}>{group.name}</option>
-          ))
-        }
+      <select onChange={setTaskGroup} value={task.group}>
+        {groups.map((group) => (
+          <option key={group.id} value={group.id}>
+            {group.name}
+          </option>
+        ))}
       </select>
     </div>
 
@@ -26,21 +39,40 @@ const TaskDetail = ({ id, task, isCompleted, comments, groups }) => (
         <button>Done</button>
       </Link>
     </div>
-
   </div>
 );
 
 const mapStateToProps = (state, ownProps) => {
   let id = ownProps.match.params.id;
-  let task = state.tasks.find(task => task.id === id);
+  let task = state.tasks.find((task) => task.id === id);
   let groups = state.groups;
   // let comments = state.comments.filter(comment => comment.task === id);
   return {
     id,
     task,
-    isCompleted: task.isCompleted,
+    isComplete: task.isComplete,
     groups,
   };
 };
 
-export const ConnectedTaskDetail = connect(mapStateToProps)(TaskDetail);
+const mapDispatchToProps = (dispatch, ownProps) => {
+  const id = ownProps.match.params.id;
+  return {
+    setTaskCompletion(id, isComplete) {
+      dispatch(mutations.setTaskCompletion(id, isComplete));
+    },
+    setTaskGroup(e) {
+      console.log("setTaskGroup", e.target.value);
+      console.log("id", id);
+      dispatch(mutations.setTaskGroup(id, e.target.value));
+    },
+    setTaskName(e) {
+      dispatch(mutations.setTaskName(id, e.target.value));
+    },
+  };
+};
+
+export const ConnectedTaskDetail = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TaskDetail);
--- src/app/store/index.js
+++ src/app/store/index.js
@@ -1,26 +1,49 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux';
-import { defaultState } from '../../server/defaultState';
-import createSagaMiddleware from 'redux-saga';
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import { defaultState } from "../../server/defaultState";
+import createSagaMiddleware from "redux-saga";
 
-import { createLogger } from 'redux-logger';
-
+import { createLogger } from "redux-logger";
 
 const sagaMiddleware = createSagaMiddleware();
-import * as sagas from './saga.mock';
-import * as mutations from './mutations';
+import * as sagas from "./saga.mock";
+import * as mutations from "./mutations";
 
 export const store = createStore(
   combineReducers({
     tasks(tasks = defaultState.tasks, action) {
       switch (action.type) {
         case mutations.CREATE_TASK:
-          return [...tasks, {
-            id: action.taskID,
-            name: "New Task",
-            group: action.groupID,
-            owner: action.ownerID,
-            isComplete: false,
-          }];
+          return [
+            ...tasks,
+            {
+              id: action.taskID,
+              name: "New Task",
+              group: action.groupID,
+              owner: action.ownerID,
+              isComplete: false,
+            },
+          ];
+
+        case mutations.SET_TASK_COMPLETE:
+          return tasks.map((task) => {
+            return task.id === action.taskID
+              ? { ...task, isComplete: action.isComplete }
+              : task;
+          });
+
+        case mutations.SET_TASK_NAME:
+          return tasks.map((task) => {
+            return task.id === action.taskID
+              ? { ...task, name: action.name }
+              : task;
+          });
+
+        case mutations.SET_TASK_GROUP:
+          return tasks.map((task) => {
+            return task.id === action.taskID
+              ? { ...task, group: action.groupID }
+              : task;
+          });
       }
       return tasks;
     },
